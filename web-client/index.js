9a25a6d054c3acc551a4980c80c5e53440659eac
Andrii Zavorotnii <andrii.zavorotnii@gmail.com>
2019-02-28 16:00:16 -0800

    Make web-client more representative

--- web-client/index.js
+++ web-client/index.js
@@ -2,13 +2,21 @@ let service = require("./example.proto3_grpc_web_pb");
 
 let client = new service.CalculatorClient("http://localhost:8081");
 
-let args = new service.ComplexArgs();
-let arg1 = new service.Complex();
-arg1.setReal(10.0);
-let arg2 = new service.Complex();
-arg2.setReal(13.0);
-args.setArgList([arg1, arg2]);
-client.add(args, {}, function (err, result) {
-    console.log(err, result.getReal(), result.getImag());
-});
-console.log(client);
+global.add = function() {
+    let args = new service.ComplexArgs();
+    let arg1 = new service.Complex();
+    arg1.setReal(parseInt(document.getElementById("arg1real").value));
+    arg1.setImag(parseInt(document.getElementById("arg1imag").value));
+    let arg2 = new service.Complex();
+    arg2.setReal(parseInt(document.getElementById("arg2real").value));
+    arg2.setImag(parseInt(document.getElementById("arg2imag").value));
+    args.setArgList([arg1, arg2]);
+    client.add(args, {}, function (err, result) {
+        if (err !== null) {
+            alert(err.message);
+            return;
+        }
+        document.getElementById("result_real").value = result.getReal();
+        document.getElementById("result_imag").value = result.getImag();
+    });
+};
